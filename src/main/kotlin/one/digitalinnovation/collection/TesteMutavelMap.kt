cd2749652d4fe4eabc8c8110f142ba04b6e408de
Koji Anasawa <jonathan.anasawa@hotmail.com>
2021-08-12 00:09:22 -0300

    created a repository

---
+++ src/main/kotlin/one/digitalinnovation/collection/TesteMutavelMap.kt
@@ -0,0 +1,22 @@
+package one.digitalinnovation.collection
+
+fun main() {
+    val joao = Funcionario("Joao", 2000.0, "CLT")
+    val pedro = Funcionario("Pedro", 1500.0, "PJ")
+    val maria = Funcionario("Maria", 4000.0, "CLT")
+
+    val repositorio = Repositorio<Funcionario>()
+
+    repositorio.create(joao.nome, joao)
+    repositorio.create(pedro.nome, pedro)
+    repositorio.create(maria.nome, maria)
+
+    println(repositorio.findById(maria.nome))
+
+    println("------------------")
+    repositorio.findall().forEach { println(it) }
+
+    println("------------------")
+    repositorio.remove(maria.nome)
+    repositorio.findall().forEach { println(it) }
+}
